72416901b3575e7c436274dd4b83c0245e7a4485
Brett Yukich <brettyukich@gmail.com>
2018-07-14 10:01:36 +1200

    Implement functions to get upper and lower bounds

--- src/Service/Fee/FeeCalculator.php
+++ src/Service/Fee/FeeCalculator.php
@@ -12,6 +12,32 @@ use Lendable\Interview\Interpolation\Model\LoanApplication;
 class FeeCalculator implements FeeCalculatorInterface
 {
     /**
+     * @var array
+     */
+    private $term12 = [
+        1000 => 50,
+        2000 => 90,
+        3000 => 90,
+        4000 => 115,
+        5000 => 100,
+        6000 => 120,
+        7000 => 140,
+        8000 => 160,
+        9000 => 180,
+        10000 => 200,
+        11000 => 220,
+        12000 => 240,
+        13000 => 260,
+        14000 => 280,
+        15000 => 300,
+        16000 => 320,
+        17000 => 340,
+        18000 => 360,
+        19000 => 380,
+        20000 => 400,
+    ];
+
+    /**
      * Calculates the fee for a loan application.
      *
      * @param LoanApplication $application The loan application to
@@ -21,7 +47,31 @@ class FeeCalculator implements FeeCalculatorInterface
      */
     public function calculate(LoanApplication $application): float
     {
+        $lower = $this->getLowerBound($application);
+
+        if ($application->getAmount() == $lower) {
+            return $this->term12[$lower];
+        }
+
+        $upper = $this->getUpperBound($application);
+
+        if ($application->getAmount() == $upper) {
+            return $this->term12[$upper];
+        }
+
+        // TODO interpolate
+
         return 1.5;
+    }
+
+    private function getLowerBound(LoanApplication $application): int
+    {
+        return intval(floor($application->getAmount() / 1000) * 1000);
+    }
+
+    private function getUpperBound(LoanApplication $application): int
+    {
+        return intval(ceil($application->getAmount() / 1000) * 1000);
     }
 
 }
