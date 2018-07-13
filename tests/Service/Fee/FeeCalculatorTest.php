211d7176cfcf0eab547c3be025c622f9d66b1fec
Brett Yukich <brettyukich@gmail.com>
2018-07-14 10:18:41 +1200

    Add test for amount that requires calculation
    The correct fee amount can be manually calculated by working out
    y = mx + c for the upper and lower bound to determine the constants.
    
    For this test an fee amount that is a multiple of 5 was chosen in order
    to test the interpolation calc and not the rounding.

--- tests/Service/Fee/FeeCalculatorTest.php
+++ tests/Service/Fee/FeeCalculatorTest.php
@@ -28,4 +28,28 @@ class FeeCalculatorTest extends TestCase
     {
         $this->assertEquals(200.0, $this->calc->calculate(new LoanApplication(12, 10000.0)));
     }
+
+    public function testCalculate_whenAmountNotMultipleOf1000_thenReturnCalculatedFee()
+    {
+        /*
+         * Between 1000 and 2000:
+         *
+         * y = mx + c
+         * 50 = m * 1000 + c | 90 = m * 2000 + c
+         * (50 - c) / 1000 = m | (90 - c) / 2000 = m
+         * (50 - c) / 1000 = (90 - c) / 2000
+         * 100 - 2c = 90 - c
+         * 100 = 90 + c
+         * 10 = c
+         *
+         * 50 = m * 1000 + 10
+         * 40 = m * 1000
+         * 0.04 = m
+         *
+         * y = 0.04 * x + 10
+         * y = 0.04 * 1500 + 10
+         * y = 70
+         */
+        $this->assertEquals(70, $this->calc->calculate(new LoanApplication(12, 1500.0)));
+    }
 }
